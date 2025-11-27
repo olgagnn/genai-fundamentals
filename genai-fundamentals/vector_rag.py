062107ad2aaf04dc22d7b94d5e7c3592dfb85201
Olga Giannouchou <115019595+olgagnn@users.noreply.github.com>
2025-11-27 15:01:52 +0000

    gg

--- genai-fundamentals/vector_rag.py
+++ genai-fundamentals/vector_rag.py
@@ -45,7 +45,7 @@ query_text = "Find me movies about toys coming alive"
 
 response = rag.search(
     query_text=query_text, 
-    retriever_config={"top_k": 5}
+    retriever_config={"top_k": 5},
     return_context=True
 )
 
